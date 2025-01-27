5ecb531b77c7b01d5beaf754b16813d8d85bfbc0
Aryan1314 <marojuaryan1314@gmail.com>
2025-01-27 15:26:53 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,189 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title =  123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// var textTitle = document.getElementById('header-title')
+// console.log(textTitle);
+
+// var textData = document.getElementsByClassName('header-class')
+// console.log(textData);
+
+// textData.textContent = "Hello"
+// textData.textContent = "Hello"
+// textTitle.innerText = "Dom Manipulation Data"
+
+// textTitle.innerHTML= "<h2> Heading of Dom</h2>"
+
+// var itemClass = document.getElementsByClassName('list-group');
+// console.log(itemClass);
+
+// var itemsClassGroup = document.getElementsByClassName('list-group');
+// console.log(itemsClassGroup);
+
+// var itemsClassContainer = document.getElementsByClassName('container');
+// console.log(itemsClassContainer);
+
+// var itemsClass = document.getElementsByClassName('list-group-item');
+// console.log(itemsClass);
+// console.log(itemsClass[1]);
+// itemsClass[1].textContent = 'Hello 2';
+// itemsClass[1].style.fontWeight = '500';
+// itemsClass[1].style.backgroundColor = '#008080';
+// itemsClass[1].style.fontSize = '1.2rem';
+// itemsClass[1].style.color = '#fff';
+
+
+// for (var i= 0; i<= itemsClass.length; i++) {
+//     itemsClass[i].style.backgroundColor='#f5f5f5';
+//     itemsClass[i].style.color = '#000';
+// }
+
+
+// for(var i = 0; i < items.length; i++){
+//   items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+// var tagClass = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = '500';
+// li[1].style.backgroundColor = '#1a1a1a ';
+// li[1].style.fontSize = '1.2rem';
+// li[1].style.color = '#fff';
+
+
+// for (var i= 0; i< li.length; i++) {
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+
+
+// var classTag = document.getElementsByTagName('li');
+// console.log(classTag);
+// console.log(classTag[1]);
+// classTag[1].textContent = 'Hello 2';
+// classTag[1].style.fontWeight = 'bold';
+// classTag[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < classTag.length; i++){
+//     classTag[i].style.backgroundColor = '#000';
+//     classTag[i].style.color = '#fff';
+//     classTag[i].style.border = '5px solid #ccc';
+// }
+
+
+// var my = document.querySelector('#main-header');
+// header.style.backgroundColor = '#000';
+
+
+// var myQuery = document.querySelector('#main-header');
+// myQuery.style.borderBottom = 'solid 4px #000';
+
+
+// var myInput = document.querySelector('input');
+// myInput.value="my dataa";
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="Aryan"
+
+// var firstColor = document.querySelector('.list-group-item');
+// firstColor.style.color = '#008080';
+// firstColor.style.fontWeight = 'bold';
+
+// for(var i = 0; i<=firstColor.length; i++){
+//     firstColor[i].style.backgroundColor = '#000';
+    
+// }
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even= document.querySelectorAll('li:nth-child(even)');
+
+// for(var i = 0; i < odd.length; i++){
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor = '#ccc';
+// }
+
+
+// var formTitles = document.querySelectorAll('.title');
+// console.log(formTitles);
+
+// formTitles.textContent=" hello";
+
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even= document.querySelectorAll('li:nth-child(even)');
+
+// for(var i = 0; i < odd.length; i++){
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor = '#ccc';
+// }
+
+// var binaryNo
+// des = document.querySelector('.list-group');
+// console.log(binaryNodes.parentNode);
+
+// binaryNodes.parentNode.style.backgroundColor = '#000';
+// binaryNodes.parentNode.style.color = '#fff';
+
+
+// var itemList = document.querySelector("#items");
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+// var binaryNodes = document.querySelector('.list-group');
+// console.log(binaryNodes.parentNode.parentNode.parentNode);
+
+// console.log(binaryNodes.childNodes);
+
+// console.log(binaryNodes.children[1]);
+// binaryNodes.children[1].style.backgroundColor = '#000';
+// binaryNodes.children[1].style.color = '#fff';
+
+// console.log(binaryNodes.nextElementSibling);
+
+// var newDiv =  document.createElement('div');
+
+// newDiv.className= 'hello';
+// newDiv.id= 'helloid';
+// newDiv.setAttribute('title', 'Hello Div');
+
+// var newDivText = document.createTextNode('Hello World');
+
+
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+
+// container.insertBefore(newDiv, h1);
+
+const newContainer =  document.createElement('div');
+newContainer.className = 'bottle';
+
+
+
+
+
+
